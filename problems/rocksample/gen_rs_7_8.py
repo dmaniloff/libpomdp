55bf51d64e3ba5c2c501817855a449c1c58bd881
dmaniloff <diego.maniloff@gmail.com>
2010-04-02 02:05:20 -0500

    adding rocksample 10-10 and the generating jython scripts

---
+++ problems/rocksample/gen_rs_7_8.py
@@ -0,0 +1,23 @@
+# /** ------------------------------------------------------------------------- *
+#  * libpomdp
+#  * ========
+#  * File: gen_rs_7_8.py
+#  * Description: jython script to generate RockSample[7,8]
+#  * Copyright (c) 2009, 2010 Diego Maniloff 
+#  * W3: http://www.cs.uic.edu/~dmanilof
+#  --------------------------------------------------------------------------- */
+
+import java.io.PrintStream as PrintStream
+import rocksampleGen
+out = PrintStream("7-8/RockSample_7_8.SPUDD")
+n=7
+k= [ [2,0],
+     [0,1],
+     [3,1],
+     [6,3],
+     [2,4],
+     [3,4],
+     [5,5],
+     [1,6] ]
+apos=[0,3]
+gen = rocksampleGen(n, k, apos, out)
