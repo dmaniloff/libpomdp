2df4574193b75cf4900cfcebe7314a8e4edfdcd5
dmaniloff <diego.maniloff@gmail.com>
2010-05-18 00:53:35 -0500

    adding sparse value function representation using UJMP

---
+++ general/java/valueFunctionSparseUJMP.java
@@ -0,0 +1,64 @@
+/** ------------------------------------------------------------------------- *
+ * libpomdp
+ * ========
+ * File: 
+ * Description: value function representation using UJMP
+ * Copyright (c) 2009, 2010 Diego Maniloff 
+ * W3: http://www.cs.uic.edu/~dmanilof
+ --------------------------------------------------------------------------- */
+
+// imports
+import org.math.array.*;
+import org.ujmp.core.*;
+import org.ujmp.core.calculation.Calculation.Ret;
+import org.ujmp.core.doublematrix.impl.*;
+import java.io.*;
+
+public class valueFunctionSparseUJMP implements valueFunction, Serializable {
+    
+    // ------------------------------------------------------------------------
+    // properties
+    // ------------------------------------------------------------------------
+
+    // represent a value function via a Matrix object
+    private DefaultSparseDoubleMatrix v;
+
+    // actions associated to each alpha vector
+    private int a[];
+
+    // constructor
+    public valueFunctionSparseUJMP(DefaultSparseDoubleMatrix v, int a[]) {
+	this.v   = v; 
+	this.a   = a;
+    }
+
+    // ------------------------------------------------------------------------
+    // interface methods
+    // ------------------------------------------------------------------------
+
+    // list of actions associated with each alpha
+    public int[] getActions() {
+	return a;
+    }
+
+    // return value of a belief state
+    public double V(belState bel) {
+	DefaultSparseDoubleMatrix b = ((belStateSparseUJMP)bel).bSparse;
+	DefaultSparseDoubleMatrix dotProdsM  = (DefaultSparseDoubleMatrix) v.mtimes(b);
+	// need to convert for now to use Common.argmax
+	// there must be a way to avoid this!!
+	double dotProds[] =  dotProdsM.toDoubleArray()[0];
+	int argmax        = Common.argmax(dotProds);	
+	// save the index of the alpha that supports this belief point
+	bel.setplanid(argmax);
+	double max = dotProds[argmax];
+	return max;
+    }
+
+    // return flat value function
+    // should not need to call this
+    public double[][] getvFlat() {
+	return v.toDoubleArray();
+    }    
+
+} // 
