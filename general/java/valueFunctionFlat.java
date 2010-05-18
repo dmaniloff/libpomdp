8fc5153fbcd18c4c3d9b7a3109236468c4611db5
dmaniloff <diego.maniloff@gmail.com>
2010-05-18 00:52:02 -0500

    adding flat rep of the value function

---
+++ general/java/valueFunctionFlat.java
@@ -0,0 +1,66 @@
+/** ------------------------------------------------------------------------- *
+ * libpomdp
+ * ========
+ * File: 
+ * Description: flat representation of a value function
+ * Copyright (c) 2009, 2010 Diego Maniloff 
+ * W3: http://www.cs.uic.edu/~dmanilof
+ --------------------------------------------------------------------------- */
+
+// imports
+import org.math.array.*;
+import java.io.*;
+
+public class valueFunctionFlat implements valueFunction, Serializable {
+    
+    // ------------------------------------------------------------------------
+    // properties
+    // ------------------------------------------------------------------------
+
+    // represent a value function via an array of doubles
+    private double v[][];
+
+    // actions associated to each alpha vector
+    private int a[];
+
+    // constructor
+    public valueFunctionFlat(double v[][], int a[]) {
+	this.v   = v; 
+	this.a   = a;
+    }
+
+    // ------------------------------------------------------------------------
+    // interface methods
+    // ------------------------------------------------------------------------
+
+    // list of actions associated with each alpha
+    public int[] getActions() {
+	return a;
+    }
+
+    // return value of a belief state
+    public double V(belState bel) {
+	double b[] = bel.getbPoint();
+	double dotProds[] = LinearAlgebra.times(v, b);
+	int argmax = Common.argmax(dotProds);
+	// save the index of the alpha that supports this belief point
+	bel.setplanid(argmax);
+	double max = dotProds[argmax];
+	return max;
+    }
+
+    // return flat value function
+    public double[][] getvFlat() {
+	return v;
+    }    
+
+    // direct contol using this value function as a policy
+    // actions start from 0
+    public int directControl(belState bel) {
+	double b[] = bel.getbPoint();
+	double dotProds[] = LinearAlgebra.times(v, b);
+	int argmax = Common.argmax(dotProds);
+	return a[argmax];
+    }
+
+} // valueFunctionFlat
