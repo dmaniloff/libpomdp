490e8665bd02ea39d95259be8534b4d7b085d9df
dmaniloff <diego.maniloff@gmail.com>
2010-06-23 10:08:18 -0500

    adding generator script for catch rectangular 5x5 with rand wumpus and adj obs

---
+++ problems/catch/gen_catch_rect_5_5_rand_adjobs.py
@@ -0,0 +1,30 @@
+'''
+ libpomdp
+ ========
+ File: genscript.py
+ Description: jython script to generate a catch SPUDD file
+              for a 5x5 rectangular grid catchw world with
+              a random wumpus and an agent with adjacent-cell
+              sensing capabilities.
+'''
+
+# imports
+import CatchGen
+import Wumpus
+import RandomWumpus
+import Sensor
+import AdjacentObs
+import CatchGridProperties
+import CatchRectangularGrid
+import java.io.PrintStream as PrintStream
+
+# declarations
+OL   = 0.5                      # omniscience level 
+gp   = CatchRectangularGrid(5, 5)
+w    = OmniWumpus(gp, OL)
+s    = AdjacentObs(gp)
+out  = PrintStream("catch_rect_5_5_rand_adjobs.SPUDD")
+gen  = CatchGen(5, 5, gp, w, s, out)
+
+# generate model
+gen.generate();
