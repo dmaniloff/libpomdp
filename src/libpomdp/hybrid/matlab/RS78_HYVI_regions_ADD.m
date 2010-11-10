904059dd733104e8d03fc587c97d6a32131faded
dmaniloff <diego.maniloff@gmail.com>
2010-11-10 15:47:58 -0500

    new decision rule, n = |support(index(b*))| / (p-k) * p * entropy(b*) > \gamma(t_c) k

--- src/libpomdp/hybrid/matlab/RS78_HYVI_regions_ADD.m
+++ src/libpomdp/hybrid/matlab/RS78_HYVI_regions_ADD.m
@@ -45,8 +45,8 @@ drawer            = rocksampleGraph;
 NUM_ROCKS         = size(ROCK_POSITIONS,1);
 
 % general parameters
-
-EXPANSION_RATE       = 80; % calculated from the online simulator, avg
+EXPANSION_RATE       = 100; % calculated from the online simulator, avg 
+AVG_EP_LIFETIME      = 29; % calculated from the online simulator, avg
 EPSILON_ACT_TH       = 1e-3;
 EPISODECOUNT         = 10;
 MAXEPISODELENGTH     = 100;
@@ -55,6 +55,9 @@ EXPANSIONTIME        = 0.9;
 BACKUPTIME           = 0.1;
 TOTALPLANNINGTIME    = 1.0;  
 USE_FACTORED_BELIEFS = 1;
+P                    = EXPANSION_RATE * TOTALPLANNINGTIME;
+K                    = EXPANSION_RATE * BACKUPTIME;
+PMK                  = P - K;
 
 logFilename = sprintf('simulation-logs/rocksample/RS78-HYVI-regions-ADD-%s.log', date);
 
@@ -115,7 +118,6 @@ for run = 1:TOTALRUNS
         end
         
         % re - initialize tree at starting belief with original bounds
-        aoTree = [];
         aoTree = AndOrTreeUpdateAdd(factoredProb, aems2h, dosih, lBound, uBound);
         aoTree.init(b_init);
         rootNode = aoTree.getRoot();
@@ -140,8 +142,8 @@ for run = 1:TOTALRUNS
             tc = cell(factoredProb.printS(factoredS));
             fprintf(1, 'Current world state is:         %s\n', tc{1});
             drawer.drawState(GRID_SIZE, ROCK_POSITIONS, factoredS);
-            if rootNode.belief.getClass.toString == ...
-                  'class libpomdp.general.java.BelStateFactoredADD'
+            if strcmp(rootNode.belief.getClass.toString, ...
+                      'class libpomdp.general.java.BelStateFactoredADD')
               fprintf(1, 'Current belief agree prob:      %d\n', ...                       
                       OP.evalN(rootNode.belief.marginals, factoredS));
             else
@@ -175,8 +177,13 @@ for run = 1:TOTALRUNS
             % will go with a first approximation, and assume that ALL nodes
             % supported by alpha-vec i will get improved if we have a candidate
             % node supported by that alpha-vec i whose I(b) > 0
-            % with this, the comparison we make is of k = exp_rate * t_bak,
+            % we will also assume that such improvement will be forever from
+            % this time-step on, that is for all t > t_c
+            % with this, the comparison we make is 
+            % p n_b?c >  ?H ?(tc) k 
+            % of k = exp_rate * t_bak,
             % is  k > max_i { #supportList[i] * exists(bakCandidate[i] } ?
+            % I DON"T UNDERSTAND THIS (and i wrote it):
             % throughout, we are also assuming (to our disadvantage) that
             % the additional k expansions we can get in t_bak would all be of
             % nodes that we can reuse
@@ -184,33 +191,66 @@ for run = 1:TOTALRUNS
 
             tic
             backedup = 0;
-            % get the support list for the action we are about to output
-            % obtain the best action for the root
-            % remember that a's and o's in matlab should start from 1
-            % need to do this a priori!! otherwise currentBestAction() could
-            % change since its randomized
-            a = aoTree.currentBestAction();
-            a = a + 1;
             
-            % sort the support list for action a
-            [sortedSupportList, oid] = sort(aoTree.fringeSupportLists(a,:), 'descend');
-            for salphaid=1:aoTree.getLB().getSize()
-                % if there exists a candidate, with I(b) > 0, and its fellow
-                % nodes outnumber the expansions that could be made, backup
-                if (rootNode.children(a).bakHeuristicStar(oid(salphaid)) > 0 && ...
-                        sortedSupportList(salphaid) > EXPANSION_RATE * BACKUPTIME)
+            % work at the root level, should not need currentBestAction anymore
+            % we will compute |V| of these f's, having kept track of I^*(b) is
+            % enough, since |support(index(b))| / (p - k) is invariant for every
+            % b in the same support set:
+            % f(b) = ?^{d^b_T} I(b) |support(index(b))| / (p - k), b ? I(T).
+            n_star           = double(aoTree.treeSupportSetSize) ./ rootNode.subTreeSize; % fraction of nodes that will improve
+            f                = rootNode.bakHeuristicStar .* double(n_star);
+            % get f* and the associated index
+            [f_star, i_star] = max(f);
+            
+            % before continuing, make sure there is a feasible candidate
+            if rootNode.bakHeuristicStar(i_star) > 0
+                % get p * n_b*_c
+                imp_n            = P * n_star(i_star); % number of nodes that will improve
+                b_star_entropy   = -OP.dotProductNoMem(...
+                    OP.log(OP.multN(...
+                    rootNode.bakCandidate(i_star).belief.marginals(1:factoredProb.getnrStaV))),...
+                    OP.multN(...
+                    rootNode.bakCandidate(i_star).belief.marginals(1:factoredProb.getnrStaV)),...
+                    factoredProb.getstaIds())...
+                    / log(factoredProb.getnrSta);
+                imp_n            = imp_n * b_star_entropy; % reduce this by the entropy of the belief to backup
+                % compute \gamma_H(t_c)
+                gamma_tc = (1 - factoredProb.getGamma()) / ...
+                    (factoredProb.getGamma() - factoredProb.getGamma()^ ...
+                    (max(AVG_EP_LIFETIME,iter) - iter + 1)); % ie., AVG_EP_LIFETIME - iter + 1 
+                % gamma_tc * K
+                % if there exists a candidate, with I(b) > 0, and it fulfills the
+                % decision rule, back it up
+                if imp_n > gamma_tc * K
                     % compute backup
                     tic
-                    aoTree.backupLowerAtNode(...
-                        rootNode.children(a).bakCandidate(oid(salphaid)));
+                    aoTree.backupLowerAtNode(rootNode.bakCandidate(i_star));
                     all.stats{run}.ep{ep}.backuptime(end+1) = toc;
                     bakC = bakC + 1;
                     % break loop
                     backedup = 1;
-                    break;
                 end
-            end
-            falseheurtime = toc
+                falseheurtime = toc;
+            end               
+            % sort the support list for action a
+%             [sortedSupportList, oid] = sort(aoTree.fringeSupportLists(a,:), 'descend');
+%             for salphaid=1:aoTree.getLB().getSize()
+%                 % if there exists a candidate, with I(b) > 0, and its fellow
+%                 % nodes outnumber the expansions that could be made, backup
+%                 if (rootNode.children(a).bakHeuristicStar(oid(salphaid)) > 0 && ...
+%                         sortedSupportList(salphaid) > EXPANSION_RATE * BACKUPTIME)
+%                     % compute backup
+%                     tic
+%                     aoTree.backupLowerAtNode(...
+%                         rootNode.children(a).bakCandidate(oid(salphaid)));
+%                     all.stats{run}.ep{ep}.backuptime(end+1) = toc;
+%                     bakC = bakC + 1;
+%                     % break loop
+%                     backedup = 1;
+%                     break;
+%                 end
+%             end
+%             falseheurtime = toc
             
 %             if(aoTree.expectedReuseRatio() > REUSETHRESHOLD && ...
 %                     rootNode.children(aoTree.currentBestAction() + 1).bakHeuristicStar > 0)
@@ -244,7 +284,13 @@ for run = 1:TOTALRUNS
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             % at this point, TOTALPLANNINGTIME has elapsed
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
-            
+            % get the support list for the action we are about to output
+            % obtain the best action for the root
+            % remember that a's and o's in matlab should start from 1
+            % need to do this a priori!! otherwise currentBestAction() could
+            % change since its randomized
+            a = aoTree.currentBestAction();
+            a = a + 1;
             
             % check whether we found an e-optimal action, either in the
             % EXPANSIONTIME slot or in the TOTALPLANNINGTIME slot
