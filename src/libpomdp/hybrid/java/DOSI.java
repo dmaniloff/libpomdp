5b2c0beabe99c09ff44fd200776db853654eba10
dmaniloff <diego.maniloff@gmail.com>
2010-11-10 15:37:50 -0500

    cleaning up, keeping track of I(b) stays the same as multiple references per support set

--- src/libpomdp/hybrid/java/DOSI.java
+++ src/libpomdp/hybrid/java/DOSI.java
@@ -28,36 +28,6 @@ public class DOSI implements backupHeuristic {
 	return o.oneStepDeltaLower * Math.pow(problem.getGamma(), o.getdepth());
     }
 
-    /// initial computation of the 
-    /// reference to best backup candidate
-    /// no need for a counterpart with an orNode argument
-//    public orNode bakStar(andNode a) {
-//	// form array with bakHeuristic
-//	double bh[] = new double[problem.getnrObs()];
-//	for(orNode o : a.children) bh[o.getobs()] = o.bakHeuristic;	
-//	return a.children[Common.argmax(bh)];
-//
-//	// STILL TO THINK ABOUT:
-//	// 	for(int o=0; o<bh.length; o++) {
-//	// 	    if(a.children[o]!=null) {
-//	// 		bh[o] = a.children[o].bakHeuristic;	
-//	// 		System.out.println("its NOT null");
-//	// 	    } else {
-//	// 		bh[o] = -1; // set the bak heuristic as that of a fringe node
-//	// 		System.out.println("its null");
-//	// 	    }
-//	// 	}
-//	// 	System.out.println(DoubleArray.toString(bh));
-//	// 	return a.children[Common.argmax(bh)];
-//    } 
-
-    // set bakHeuristic value of this andNode right
-    // after its creation
-    // in the future this may allow for the introduction of a weighting coeff
-//    public double bakHStar(andNode a, int i) {
-//	return a.bakCandidate[i].bakHeuristicStar[i] * 1; // weight here
-//    }
-
     /// compare current bakCandidate with the child's
     /// on the updated branch
     /// now takes the index of the alpha vec to know what region we are comparing
