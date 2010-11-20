d0ce55a757b932a263f9187422cc0695b4bd678d
dmaniloff <diego.maniloff@gmail.com>
2010-11-20 00:16:33 -0500

    adjusting exp rate and avg lifetime for rs1010 hyvi simulator

--- src/libpomdp/hybrid/matlab/RS1010_HYVI_regions_ADD.m
+++ src/libpomdp/hybrid/matlab/RS1010_HYVI_regions_ADD.m
@@ -50,8 +50,8 @@ NUM_ROCKS         = size(ROCK_POSITIONS,1);
 
 % general parameters
 
-EXPANSION_RATE       = 30; % calculated from the online simulator, avg 
-AVG_EP_LIFETIME      = 26; % calculated from the online simulator, avg
+EXPANSION_RATE       = 25; % calculated from the online simulator, avg 
+AVG_EP_LIFETIME      = 30; % calculated from the online simulator, avg
 EPSILON_ACT_TH       = 1e-3;
 EPISODECOUNT         = 5;
 MAXEPISODELENGTH     = 100;
