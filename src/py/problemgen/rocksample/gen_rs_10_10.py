b73857e3a3147de768f6427d09757174f38dde8b
dmaniloff <diego.maniloff@gmail.com>
2011-11-19 20:07:03 -0500

    moving .pomdp version of rs78. updating .jy problemgen scripts. .

--- src/py/problemgen/rocksample/gen_rs_10_10.py
+++ src/py/problemgen/rocksample/gen_rs_10_10.py
@@ -1,25 +1,31 @@
-# /** ------------------------------------------------------------------------- *
-#  * libpomdp
-#  * ========
-#  * File: gen_rs_10_10.py
-#  * Description: jython script to generate RockSample[10,10]
-#  * Copyright (c) 2009, 2010 Diego Maniloff 
-#  * W3: http://www.cs.uic.edu/~dmanilof
-#  --------------------------------------------------------------------------- */
+'''
+ libpomdp
+ ========
+ File: gen_rs_10_10.py
+ Description: jython script to generate RockSample[10,10]
+ Copyright (c) 2009, 2010, 2011 Diego Maniloff 
+'''
 
+# imports
+import sys
+sys.path.append('../../../../dist/libpomdp.jar')
 import java.io.PrintStream as PrintStream
-import rocksampleGen
+from libpomdp.problems.rocksample.java import *
+
+# declarations
 out = PrintStream("10-10/RockSample_10_10.SPUDD")
-n=10
-k= [ [0, 3],
-     [0, 7],
-     [1, 8],
-     [3, 3],
-     [3, 8],
-     [4, 3],
-     [5, 8],
-     [6, 1],
-     [9, 3],
-     [9, 9], ]
-apos=[0,5]
-gen = rocksampleGen(n, k, apos, out)
+n   = 10
+k   = [ [0, 3],
+        [0, 7],
+        [1, 8],
+        [3, 3],
+        [3, 8],
+        [4, 3],
+        [5, 8],
+        [6, 1],
+        [9, 3],
+        [9, 9], ]
+apos = [0,5]
+
+# generate
+gen = RocksampleGen(n, k, apos, out)
